93a98470d2c527e769e2e5c8fbdade8236f3b3d5
Krystal Jean Austin <44277065+KrystalJean@users.noreply.github.com>
2022-09-19 19:12:37 -0500

    added images and audio clips

---
+++ save.js
@@ -0,0 +1,36 @@
+ // switch (randomNumberArray[i]) {
+        //     case 1: soundClip = clip1;
+        //         break;
+        //     case 2: soundClip = './audio/cow.mp3';
+        //         break;
+        //     case 3: soundClip = 'https://www.computerhope.com/jargon/m/example11111111.mp3';
+        //         break;
+        //     case 4: soundClip = 'https://www.computerhope.com/jargon/m/example11111111.mp3';
+        //         break;
+        //     case 5: soundClip = 'https://www.computerhope.com/jargon/m/example11111111.mp3';
+        //         break;
+        //     case 6: soundClip = 'https://www.computerhope.com/jargon/m/example11111111.mp3';
+        //         break;
+        //     case 7: soundClip = 'https://www.computerhope.com/jargon/m/example7777777777777.mp3';
+        //         break;
+        //     case 8: soundClip = 'https://www.computerhope.com/jargon/m/example888888888888888.mp3';
+        //         break;
+        //     case 9: soundClip = 'https://www.computerhope.com/jargon/m/example888888888888888.mp3';
+        //         break;
+        //     case 10: soundClip = 'https://www.computerhope.com/jargon/m/example888888888888888.mp3';
+        //         break;
+        //     case 11: soundClip = 'https://www.computerhope.com/jargon/m/example888888888888888.mp3';
+        //         break;
+        //     case 12: soundClip = 'https://www.computerhope.com/jargon/m/example888888888888888.mp3';
+        //         break;
+        //     case 13: soundClip = 'https://www.computerhope.com/jargon/m/example888888888888888.mp3';
+        //         break;
+        //     case 14: soundClip = 'https://www.computerhope.com/jargon/m/example888888888888888.mp3';
+        //         break;
+        //     case 15: soundClip = 'https://www.computerhope.com/jargon/m/example888888888888888.mp3';
+        //         break;
+        //     case 16: soundClip = 'https://www.computerhope.com/jargon/m/example888888888888888.mp3';
+        //         break;
+        //     default:
+        //         console.log("No value found");
+        // }
