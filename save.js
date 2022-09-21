b4548b702181da3a3c4c83c729f99e9b902e4546
Krystal Jean Austin <44277065+KrystalJean@users.noreply.github.com>
2022-09-20 22:58:59 -0500

    Danger: saving now cuz im super tired

--- save.js
+++ save.js
@@ -55,3 +55,125 @@
 // const btn14 = document.getElementById('guess-btn-14');
 // const btn15 = document.getElementById('guess-btn-15');
 // const btn16 = document.getElementById('guess-btn-16');
+
+
+
+
+        // switch (button) {
+
+        //     case buttonPadArray[0]:
+        //         sound1.play();
+        //         break;
+
+        //     case btn2:
+        //         sound2.play();
+        //         break;
+
+        //     case btn3:
+        //         sound3.play();
+        //         break;
+
+        //     case btn4:
+        //         sound4.play();
+        //         break;
+
+        //     case btn5:
+        //         sound5.play();
+        //         break;
+        //     case btn6:
+        //         sound6.play();
+        //         break;
+        //     case btn7:
+        //         sound7.play();
+        //         break;
+        //     case btn8:
+        //         sound8.play();
+        //         break;
+        //     case btn9:
+        //         sound9.play();
+        //         break;
+        //     case btn10:
+        //         sound10.play();
+        //         break;
+        //     case btn11:
+        //         sound11.play();
+        //         break;
+        //     case btn12:
+        //         sound12.play();
+        //         break;
+        //     case btn13:
+        //         sound13.play();
+        //         break;
+        //     case btn14:
+        //         sound14.play();
+        //         break;
+        //     case btn15:
+        //         sound15.play();
+        //         break;
+        //     case btn16:
+        //         sound16.play();
+        //         break;
+        //     default:
+        //         console.log('invalid');
+        //         break;
+        // }
+
+
+
+
+        // function setHenryButton() {
+
+        //         for (let i = 0; i < n; i++) {
+            
+        //             if (henrySoundsArray[i] === i++) {
+        //                 henrySound.src = clip1;
+        //                 // console.log(henrySound.src);
+        //             } else if (henrySoundsArray[i] === 2) {
+        //                 henrySound.src = clip2;
+        //                 // console.log(henrySound.src);
+        //             } else if (henrySoundsArray[i] === 3) {
+        //                 henrySound.src = clip3;
+        //                 // console.log(henrySound.src);
+        //             } else if (henrySoundsArray[i] === 4) {
+        //                 henrySound.src = clip4;
+        //                 // console.log(henrySound.src);
+        //             } else if (henrySoundsArray[i] === 5) {
+        //                 henrySound.src = clip5;
+        //                 // console.log(henrySound.src);
+        //             } else if (henrySoundsArray[i] === 6) {
+        //                 henrySound.src = clip6;
+        //                 // console.log(henrySound.src);
+        //             } else if (henrySoundsArray[i] === 7) {
+        //                 henrySound.src = clip7;
+        //                 // console.log(henrySound.src);
+        //             } else if (henrySoundsArray[i] === 8) {
+        //                 henrySound.src = clip8;
+        //                 // console.log(henrySound.src);
+        //             } else if (henrySoundsArray[i] === 9) {
+        //                 henrySound.src = clip9;
+        //                 // console.log(henrySound.src);
+        //             } else if (henrySoundsArray[i] === 10) {
+        //                 henrySound.src = clip10;
+        //                 // console.log(henrySound.src);
+        //             } else if (henrySoundsArray[i] === 11) {
+        //                 henrySound.src = clip11;
+        //                 // console.log(henrySound.src);
+        //             } else if (henrySoundsArray[i] === 12) {
+        //                 henrySound.src = clip12;
+        //                 // console.log(henrySound.src);
+        //             } else if (henrySoundsArray[i] === 13) {
+        //                 henrySound.src = clip13;
+        //                 // console.log(henrySound.src);
+        //             } else if (henrySoundsArray[i] === 14) {
+        //                 henrySound.src = clip14;
+        //                 // console.log(henrySound.src);
+        //             } else if (henrySoundsArray[i] === 15) {
+        //                 henrySound.src = clip15;
+        //                 // console.log(henrySound.src);
+        //             } else if (henrySoundsArray[i] === 16) {
+        //                 henrySound.src = clip16;
+        //                 // console.log(henrySound.src);
+        //             } else {
+        //                 // console.log("none");
+        //             }
+        //         }
--- script.js
+++ script.js
@@ -54,12 +54,14 @@ const clip16 = './audio/cow.mp3';
 
 let randomNumberArray = [];
 let henrySoundsArray = [];
-const buttonPad = [btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11, btn12, btn13, btn14, btn15, btn16];
-const sounds = [sound1, sound2, sound3, sound4, sound5, sound6, sound7, sound8, sound9, sound10, sound11, sound12, sound13, sound14, sound15, sound16];
+const clipArray = [clip1, clip2, clip3, clip4, clip5, clip6, clip7, clip8, clip9, clip10, clip11, clip12, clip13, clip14, clip15, clip16];
+const buttonPadArray = [btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11, btn12, btn13, btn14, btn15, btn16];
+const soundsArray = [sound1, sound2, sound3, sound4, sound5, sound6, sound7, sound8, sound9, sound10, sound11, sound12, sound13, sound14, sound15, sound16];
 const buttons = document.querySelectorAll('.guess-btn');
 const henryBtn = document.getElementById('henry-btn');
 const startBtn = document.getElementById('start-new-game');
 const henrySound = document.getElementById('henry-sound');
+
 
 
 const n = 16; //number of guess buttons, and length of random numbers array
@@ -73,7 +75,6 @@ function getRandomNumberArray() {
 
     } while (randomNumberArray.length < n);
 }
-
 
 function getHenrySoundArray() {
     do {
@@ -140,49 +141,47 @@ function setHenryButton() {
             henrySound.src = clip16;
             // console.log(henrySound.src);
         } else {
-            // console.log("none");
+            console.log("none");
         }
     }
-
-
 }
 
 function setButtonPad() {
     for (let i = 0; i < n; i++) {
         if (randomNumberArray[i] === 1) {
-            sounds[i].src = clip1;
+            soundsArray[i].src = clip1;
         } else if (randomNumberArray[i] === 2) {
-            sounds[i].src = clip2;
+            soundsArray[i].src = clip2;
         } else if (randomNumberArray[i] === 3) {
-            sounds[i].src = clip3;
+            soundsArray[i].src = clip3;
         } else if (randomNumberArray[i] === 4) {
-            sounds[i].src = clip4;
+            soundsArray[i].src = clip4;
         } else if (randomNumberArray[i] === 5) {
-            sounds[i].src = clip5;
+            soundsArray[i].src = clip5;
         } else if (randomNumberArray[i] === 6) {
-            sounds[i].src = clip6;
+            soundsArray[i].src = clip6;
         } else if (randomNumberArray[i] === 7) {
-            sounds[i].src = clip7;
+            soundsArray[i].src = clip7;
         } else if (randomNumberArray[i] === 8) {
-            sounds[i].src = clip8;
+            soundsArray[i].src = clip8;
         } else if (randomNumberArray[i] === 9) {
-            sounds[i].src = clip9;
+            soundsArray[i].src = clip9;
         } else if (randomNumberArray[i] === 10) {
-            sounds[i].src = clip10;
+            soundsArray[i].src = clip10;
         } else if (randomNumberArray[i] === 11) {
-            sounds[i].src = clip11;
+            soundsArray[i].src = clip11;
         } else if (randomNumberArray[i] === 12) {
-            sounds[i].src = clip12;
+            soundsArray[i].src = clip12;
         } else if (randomNumberArray[i] === 13) {
-            sounds[i].src = clip13;
+            soundsArray[i].src = clip13;
         } else if (randomNumberArray[i] === 14) {
-            sounds[i].src = clip14;
+            soundsArray[i].src = clip14;
         } else if (randomNumberArray[i] === 15) {
-            sounds[i].src = clip15;
+            soundsArray[i].src = clip15;
         } else if (randomNumberArray[i] === 16) {
-            sounds[i].src = clip16;
+            soundsArray[i].src = clip16;
         } else {
-            // console.log("none");
+            console.log("none");
         }
     }
 }
@@ -190,10 +189,12 @@ function setButtonPad() {
 function startNewGame() {
     randomNumberArray = [];
     henrySoundsArray = []
+
     getRandomNumberArray();
-    getHenrySoundArray()
+    getHenrySoundArray();
     setHenryButton();
     setButtonPad();
+
 
     const demo = document.getElementById('demo');
     const playersTurn = document.getElementById('player-indicator');
@@ -212,88 +213,38 @@ startBtn.addEventListener("click", () => {
     startNewGame();
 });
 
+
+
 //button pad event listeners
 buttons.forEach(button => {
     button.addEventListener("click", () => {
-        switch (button) {
-            case btn1:
-                sound1.play();
+        for (let i = 0; i < n; i++) {
 
-                console.log('starttttttttttttt');
-                console.log(sound1.src);
-                console.log(henrySound.src);
-                if (sound1.src === henrySound.src) {
-                    console.log('correct');
-                    setHenryButton()
+            if (button === buttonPadArray[i]) {
+                soundsArray[i].play();
+
+
+                if (soundsArray[i].src === henrySound.src) {
+                    console.log('Correct');
+                    button.style.backgroundColor = 'red';                  
+                    button.disabled = true;
+                    henrySound.src = clipArray[i];
+
+
                 } else {
-                    console.log('incorrect');
+                    console.log('Incorrect');
                 }
-                break;
-                
-            case btn2:
-                sound2.play();
-                break;
-            case btn3:
-                sound3.play();
-                break;
-            case btn4:
-                sound4.play();
-                break;
-            case btn5:
-                sound5.play();
-                break;
-            case btn6:
-                sound6.play();
-                break;
-            case btn7:
-                sound7.play();
-                break;
-            case btn8:
-                sound8.play();
-                break;
-            case btn9:
-                sound9.play();
-                break;
-            case btn10:
-                sound10.play();
-                break;
-            case btn11:
-                sound11.play();
-                break;
-            case btn12:
-                sound12.play();
-                break;
-            case btn13:
-                sound13.play();
-                break;
-            case btn14:
-                sound14.play();
-                break;
-            case btn15:
-                sound15.play();
-                break;
-            case btn16:
-                sound16.play();
-                break;
-            default:
-                // console.log('invalid');
-                break;
-        }
+            }
+        };
+
+
 
     });
 });
 
-// function lightUpButton() {
-//     if(he)
-// }
+console.log(henrySound.src);
+// console.log(randomNumberArray);
+// console.log(henrySoundsArray);
 
 
-// const player1 = x;
-// const player2 = y;
-// const computer = z;
 
-
-console.log(randomNumberArray);
-console.log(henrySoundsArray);
-// console.log(sound1.src);
-// console.log(henrySound.src);
