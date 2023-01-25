8e746db041885de87699e2f6260159628f78b284
kaustin <krystalaustin99@gmail.com>
2023-01-25 12:34:58 -0600

    refactored hardcoded arrays in js into class arrays

--- script.js
+++ script.js
@@ -1,62 +1,30 @@
-//guessButtons to click when guessing an answer 
-const btn1 = document.getElementById('guess-btn-1');
-const btn2 = document.getElementById('guess-btn-2');
-const btn3 = document.getElementById('guess-btn-3');
-const btn4 = document.getElementById('guess-btn-4');
-const btn5 = document.getElementById('guess-btn-5');
-const btn6 = document.getElementById('guess-btn-6');
-const btn7 = document.getElementById('guess-btn-7');
-const btn8 = document.getElementById('guess-btn-8');
-const btn9 = document.getElementById('guess-btn-9');
-const btn10 = document.getElementById('guess-btn-10');
-const btn11 = document.getElementById('guess-btn-11');
-const btn12 = document.getElementById('guess-btn-12');
-const btn13 = document.getElementById('guess-btn-13');
-const btn14 = document.getElementById('guess-btn-14');
-const btn15 = document.getElementById('guess-btn-15');
-const btn16 = document.getElementById('guess-btn-16');
+//individual sound clip files
+const clipArray = [
+    './audio/beep.mp3',
+    './audio/cow.mp3',
+    './audio/doorbell.mp3',
+    './audio/glass.mp3',
+    './audio/hawk.mp3',
+    './audio/hey.mp3',
+    './audio/horse.mp3',
+    './audio/kiss.mp3',
+    './audio/owl.mp3',
+    './audio/phone.mp3',
+    './audio/punch.mp3',
+    './audio/rooster.mp3',
+    './audio/tada.mp3',
+    './audio/window.mp3',
+    './audio/dog.mp3',
+    './audio/pig.mp3',
+    './audio/trumpet.mp3'
+]
+
+//guessButtons - to click when guessing an answer 
+const btnPadArray = document.querySelectorAll('.guess-btn');
 
 //used to access the button audio source attribute's value
-const sound1 = document.getElementById('sound-1');
-const sound2 = document.getElementById('sound-2');
-const sound3 = document.getElementById('sound-3');
-const sound4 = document.getElementById('sound-4');
-const sound5 = document.getElementById('sound-5');
-const sound6 = document.getElementById('sound-6');
-const sound7 = document.getElementById('sound-7');
-const sound8 = document.getElementById('sound-8');
-const sound9 = document.getElementById('sound-9');
-const sound10 = document.getElementById('sound-10');
-const sound11 = document.getElementById('sound-11');
-const sound12 = document.getElementById('sound-12');
-const sound13 = document.getElementById('sound-13');
-const sound14 = document.getElementById('sound-14');
-const sound15 = document.getElementById('sound-15');
-const sound16 = document.getElementById('sound-16');
+const soundsArray = document.querySelectorAll('.sound');
 
-//individual sound clip files
-let clip1 = './audio/beep.mp3';
-let clip2 = './audio/cow.mp3';
-let clip3 = './audio/doorbell.mp3';
-let clip4 = './audio/glass.mp3';
-let clip5 = './audio/hawk.mp3';
-let clip6 = './audio/hey.mp3';
-let clip7 = './audio/horse.mp3';
-let clip8 = './audio/kiss.mp3';
-let clip9 = './audio/owl.mp3';
-let clip10 = './audio/phone.mp3';
-let clip11 = './audio/punch.mp3';
-let clip12 = './audio/rooster.mp3';
-let clip13 = './audio/tada.mp3';
-let clip14 = './audio/window.mp3';
-let clip15 = './audio/dog.mp3';
-let clip16 = './audio/pig.mp3';
-let clip17 = './audio/trumpet.mp3';
-
-
-const clipArray = [clip1, clip2, clip3, clip4, clip5, clip6, clip7, clip8, clip9, clip10, clip11, clip12, clip13, clip14, clip15, clip16, clip17];
-const btnPadArray = [btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11, btn12, btn13, btn14, btn15, btn16];
-const soundsArray = [sound1, sound2, sound3, sound4, sound5, sound6, sound7, sound8, sound9, sound10, sound11, sound12, sound13, sound14, sound15, sound16];
 let henrySoundsArray = [];
 let henryIncrementor = 1;
 let henrySound = document.getElementById('henry-sound');
@@ -218,14 +186,14 @@ themeOne.addEventListener("click", () => {
     buttonPad.className = 'theme-one';
     let guessBtnVarColor = getComputedStyle(document.querySelector('.theme-one'))
         .getPropertyValue('--guess-btn-bg-color');
-    
+
     let switchToggleVarColor = getComputedStyle(document.querySelector('.theme-one'))
         .getPropertyValue('--switch-toggle-bg-color');
 
     guessButtons.forEach(guessButton => {
         guessButton.style.backgroundColor = guessBtnVarColor;
     })
-    
+
     switchToggles.forEach(switchToggle => {
         switchToggle.style.backgroundColor = switchToggleVarColor;
     })
@@ -242,7 +210,7 @@ themeTwo.addEventListener("click", () => {
     let guessBtnVarColor = getComputedStyle(document.querySelector('.theme-two'))
         .getPropertyValue('--guess-btn-bg-color');
 
-        let switchToggleVarColor = getComputedStyle(document.querySelector('.theme-two'))
+    let switchToggleVarColor = getComputedStyle(document.querySelector('.theme-two'))
         .getPropertyValue('--switch-toggle-bg-color');
 
     guessButtons.forEach(guessButton => {
