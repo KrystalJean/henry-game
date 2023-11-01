7ae92b74594bfbdd85f5e7e03b0b26ee2c2e5299
kaustin <krystalaustin99@gmail.com>
2023-10-31 23:51:16 -0500

    added a christmas theme

---
+++ snow.js
@@ -0,0 +1,71 @@
+document.addEventListener('DOMContentLoaded', function(){
+    function initializeParticleEffect() {
+      particlesJS("snow", {
+        "particles": {
+            "number": {
+                "value": 200,
+                "density": {
+                    "enable": true,
+                    "value_area": 800
+                }
+            },
+            "color": {
+                "value": "#ffffff"
+            },
+            "opacity": {
+                "value": 0.7,
+                "random": false,
+                "anim": {
+                    "enable": false
+                }
+            },
+            "size": {
+                "value": 5,
+                "random": true,
+                "anim": {
+                    "enable": false
+                }
+            },
+            "line_linked": {
+                "enable": false
+            },
+            "move": {
+                "enable": true,
+                "speed": 5,
+                "direction": "bottom",
+                "random": true,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false,
+                "attract": {
+                    "enable": true,
+                    "rotateX": 300,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "events": {
+                "onhover": {
+                    "enable": false
+                },
+                "onclick": {
+                    "enable": false
+                },
+                "resize": false
+            }
+        },
+        "retina_detect": true
+      });
+    }
+  
+    var script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js';
+    script.onload = function(){
+      document.getElementById('theme-3').addEventListener('click', function() {
+        initializeParticleEffect();
+      });
+    }
+    document.head.append(script);
+  });
+  
